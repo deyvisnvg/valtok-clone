8fd005c3932612201653d33b4224dfa55468d3a1
deyvisnvg <deyvisnvg@gmail.com>
2023-03-02 21:20:07 -0500

    feat(Root): Add new icons, Heart, Comment, Share

---
+++ src/components/Icons/Heart.jsx
@@ -0,0 +1,5 @@
+export const Heart = ({ fill = 'currentColor' }) => {
+    return (
+        <svg width="48" data-e2e="" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M15 4.5C21 4.5 24 8.5 24 8.5C24 8.5 27 4.5 33 4.5C40 4.5 45 9.99998 45 17C45 25 38.4622 32.1314 32.5 37.5C28.8191 40.8144 26 43 24 43C22 43 19.101 40.7978 15.5 37.5C9.63898 32.1325 3 25 3 17C3 9.99998 8 4.5 15 4.5Z" fill={fill} fill-opacity="0.9"></path></svg>
+    )
+}
